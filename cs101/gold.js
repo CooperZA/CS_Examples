0a2a8b817542f23bcf348cbe4fb3311b8177a756
Zach Cooper <zach@raincitysolutions.com>
2020-02-13 12:25:07 -0800

    updated examples

--- cs101/gold.js
+++ cs101/gold.js
@@ -8,3 +8,16 @@ const goldPrice = 1574.70;
 const goldCalc = (gold) => {
     return alert("Your gold is worth: $" + (gold * goldPrice).toFixed(2));
 }
+
+function myFunc(x, y){
+    add = x + y;
+    return add;
+}
+
+let twoNumbersAdded = myFunc(2, 3);
+
+console.log(twoNumbersAdded); // result: 5
+
+
+let len
+const fence =300;
